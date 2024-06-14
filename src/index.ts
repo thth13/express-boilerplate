7bdb68761f8889f1a46ea1d6d30aeb00e94afc90
Dmitriy Khutornoy <thth13q@gmail.com>
2024-06-14 10:42:17 +0300

    fix deploy scripts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import usersRouter from './routes/users'
+import path from 'path'
 import { port } from './config/config'
 import { connectDB } from './config/db'
 
@@ -12,6 +13,14 @@ connectDB()
 // Routes
 app.use('/api/users/', usersRouter)
 app.use('/uploads', express.static('uploads'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
